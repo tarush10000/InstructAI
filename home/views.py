db04e7209af20e20adb091b05ce79e394a28b0c5
shreyanshh24 <shreyanshkumar2411@gmail.com>
2025-03-25 00:20:47 +0530

    Authentication done

--- home/views.py
+++ home/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login  # Renamed import
+from django.contrib.auth import authenticate
+from .forms import CustomUserCreationForm
 
 def home_page(request):
     return render(request, 'home/index.html')
 
 def signup(request):
-    return render(request, 'home/signup.html')
+    if request.method == 'POST':
+        print("chal agay")
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)  # Use renamed import
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    
+    return render(request, 'home/signup.html', {'form': form})
 
-def login(request):
+def login(request):  # Renamed from 'login'
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        
+        if user is not None:
+            auth_login(request, user)  # Use renamed import
+            return redirect('home')
+        else:
+            return render(request, 'home/login.html', {'error': 'Invalid credentials'})
+    
     return render(request, 'home/login.html')
